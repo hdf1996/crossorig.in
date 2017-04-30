1e1c88befd988e2c364236c57c8084f52e4d8035
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-29 22:43:28 -0300

    Deploy testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,22 +4,18 @@ lock "3.8.1"
 set :application, "crossorig.in"
 set :repo_url, "git@github.com:hdf1986/crossorig.in.git"
 
-# Default branch is :master
 set :branch, "api/master"
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/ubuntu/crossorig.in"
 
 set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 set :rbenv_roles, :all # default value
 
 set :bundle_binstubs, -> { shared_path.join('bin') }
 
-append :rbenv_map_bins, 'puma', 'pumactl'
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
@@ -40,25 +36,4 @@ namespace :puma do
   end
 
   before :start, :make_dirs
-end
-
-namespace :deploy do
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
 end
