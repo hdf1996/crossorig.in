f2782911637f220e743dbb4e16e7b9a16ed4b016
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-01 12:09:43 -0300

    removed old capistrano info

--- Capfile
+++ Capfile
@@ -15,24 +15,4 @@ require "capistrano/deploy"
 require "capistrano/scm/git"
 install_plugin Capistrano::SCM::Git
 
-# Include tasks from other gems included in your Gemfile
-#
-# For documentation on these, see for example:
-#
-#   https://github.com/capistrano/rvm
-#   https://github.com/capistrano/rbenv
-#   https://github.com/capistrano/chruby
-#   https://github.com/capistrano/bundler
-#   https://github.com/capistrano/rails
-#   https://github.com/capistrano/passenger
-#
-# require "capistrano/rvm"
-require "capistrano/rbenv"
-
-# require "capistrano/chruby"
-require "capistrano/bundler"
-
-require 'capistrano/puma'
-
-install_plugin Capistrano::Puma  # Default puma tasks
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -16,28 +16,28 @@ set :rbenv_roles, :all # default value
 
 set :bundle_binstubs, -> { shared_path.join('bin') }
 
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_access_log, "#{release_path}/log/puma.error.log"
+# set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :puma_preload_app, true
+# set :puma_worker_timeout, nil
+# set :puma_init_active_record, false  # Change to true if using ActiveRecord
 
 set :linked_files, fetch(:linked_files, []).push('.env.production')
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
+# namespace :puma do
+#   desc 'Create Directories for Puma Pids and Socket'
+#   task :make_dirs do
+#     on roles(:app) do
+#       execute "mkdir #{shared_path}/tmp/sockets -p"
+#       execute "mkdir #{shared_path}/tmp/pids -p"
+#     end
+#   end
 
-  before :start, :make_dirs
-end
+#   before :start, :make_dirs
+# end
 
 namespace :nginx do
   task :restart do
@@ -47,4 +47,4 @@ namespace :nginx do
   end
 end
 
-after "puma:restart", "nginx:restart"
+after "deploy:finished", "nginx:restart"
