f1ffe38d6ae1f4e261a331e38c05cca58164ebc1
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-04 22:23:19 -0300

    Travis test

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+eval `ssh-agent -s`
+mkdir -p ~/.ssh
+ls ~/.ssh
+openssl aes-256-cbc -K $encrypted_10485440d0a6_key -iv $encrypted_10485440d0a6_iv -in github_deploy_key.enc -out ~/.ssh/id_rsa -d
+mv github_deploy_key.pub ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa.pub
+ssh-add ~/.ssh/id_rsa
+ssh-keyscan -H 54.70.220.103 >> ~/.ssh/known_hosts
+ssh ubuntu@54.70.220.103
