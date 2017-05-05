c0d1e395f78d1e8af7dcf5ada5d8d90acda2c1e4
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-04 22:46:33 -0300

    deploy testing

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'capistrano'
 gem 'capistrano-bundler', '~> 1.2'
+gem 'capistrano-rbenv', '~> 2.0'
 gem 'sass'
--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,14 @@ set :keep_releases, 2
 
 set :bundle_bins, fetch(:bundle_bins, []).push('sass')
 
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl sass}
+set :rbenv_roles, :all # default value
+
+set :bundle_binstubs, -> { shared_path.join('bin') }
+
 namespace :sass do
   task :compile do
     on roles(:all) do |host|
