c08352fe820fa56f00bafd5ec49657875444cf8d
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-07 18:22:25 -0300

    HTTP GET Ready

--- server/ruby/main.rb
+++ server/ruby/main.rb
@@ -7,10 +7,9 @@ set :port, 3000
 
 NON_PROXIABLE_HEADERS = ["Set-Cookie", "Connection", "Transfer-Encoding"].freeze
 
-get '/http//:route_to_proxy' do
-  @proxied_response = HTTParty.get("http://" + params['route_to_proxy'])
+get %r{/http(:)//(.*)} do
+  @proxied_response = HTTParty.get("http://" + params['captures'].last)
   @proxied_response.headers.canonical_each do |k, i|
-    puts k unless NON_PROXIABLE_HEADERS.include? k
     next if NON_PROXIABLE_HEADERS.include? k
     headers[k] = i
   end
