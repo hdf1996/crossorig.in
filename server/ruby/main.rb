3794ccf347fbc08abb9bb005e7655689aab27539
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-07 18:29:25 -0300

    Added query params

--- server/ruby/main.rb
+++ server/ruby/main.rb
@@ -7,11 +7,16 @@ set :port, 3000
 
 NON_PROXIABLE_HEADERS = ["Set-Cookie", "Connection", "Transfer-Encoding"].freeze
 
-get %r{/http(:)//(.*)} do
-  @proxied_response = HTTParty.get("http://" + params['captures'].last)
-  @proxied_response.headers.canonical_each do |k, i|
+before do
+
+end
+
+get %r{/(http|https)(:)//(.*)} do
+  proxied_response = HTTParty.get("#{params["captures"].first}://#{params['captures'].last}",
+    query: request.env['rack.request.query_hash'])
+  proxied_response.headers.canonical_each do |k, i|
     next if NON_PROXIABLE_HEADERS.include? k
     headers[k] = i
   end
-  @proxied_response.body
+  proxied_response.body
 end
