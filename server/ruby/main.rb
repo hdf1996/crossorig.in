3ee1d600b6d783ba03bade15fd09c8c51cc36d00
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-11 22:36:15 -0300

    fixed nginx on deploy

--- server/ruby/main.rb
+++ server/ruby/main.rb
@@ -12,14 +12,19 @@ configure {
   set :server, :puma
 }
 
-NON_PROXIABLE_HEADERS = ["Set-Cookie", "Connection", "Transfer-Encoding"].freeze
 
-get %r{/(http|https)(:)//(.*)} do
-  proxied_response = HTTParty.get("#{params["captures"].first}://#{params['captures'].last}",
-    query: request.env['rack.request.query_hash'])
-  proxied_response.headers.canonical_each do |k, i|
-    next if NON_PROXIABLE_HEADERS.include? k
-    headers[k] = i
+class Pumatra < Sinatra::Base
+  NON_PROXIABLE_HEADERS = ["Set-Cookie", "Connection", "Transfer-Encoding"].freeze
+
+  get %r{/(http|https)(:)//(.*)} do
+    proxied_response = HTTParty.get("#{params["captures"].first}://#{params['captures'].last}",
+      query: request.env['rack.request.query_hash'])
+    proxied_response.headers.canonical_each do |k, i|
+      next if NON_PROXIABLE_HEADERS.include? k
+      headers[k] = i
+    end
+    proxied_response.body
   end
-  proxied_response.body
+
+  run! if app_file == $0
 end
