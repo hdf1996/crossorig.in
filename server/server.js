2146d434139caaf770f8490d422ad0915e8ebe4d
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-12 22:48:38 -0300

    Added query params

--- server/server.js
+++ server/server.js
@@ -3,8 +3,12 @@ var request = require('request');
 var app = express();
 
 app.get(/(http|https)(:)\/\/(.*)/, function (req, res) {
-  var url = req.params[0] + "://" + req.params[2];
+  var url = req.url.substr(1);
   console.log("Request url GET: " + url)
+  console.log(req.url)
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Request-Method', 'GET, PATCH, PUT, POST, OPTIONS, DELETE');
+  res.header('Doge', 'SUCH CORS');
   request(url).pipe(res);
 });
 
