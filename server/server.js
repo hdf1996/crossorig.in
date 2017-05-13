51ba2180f608dc11de0e39b17906858fbfd4ffb0
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-12 22:17:41 -0300

    fixed nginx on deploy

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/(http|https)(:)(.*)', function (req, res) {
+app.get(/^\/(https?:\/\/.+)/, function (req, res) {
   res.send('Hello World!');
 });
 
