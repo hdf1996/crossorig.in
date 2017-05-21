de38980a55909a131b8f245accf212dd33415520
Hugo David Farji <hdf1986.6700@gmail.com>
2017-05-21 11:56:05 -0300

    Added post method

--- server/server.js
+++ server/server.js
@@ -30,6 +30,20 @@ app.get(/(http|https)(:)\/\/(.*)/, function (req, res) {
   }).on('response', (r) => { console.log("Finished GET (" + r.statusCode + ") " + url)}).pipe(res);
 });
 
+app.post(/(http|https)(:)\/\/(.*)/, function (req, res) {
+  var url = req.url.substr(1);
+  console.log("Request url GET: " + url)
+  console.log(req.url)
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Request-Method', 'GET, PATCH, PUT, POST, OPTIONS, DELETE');
+  res.header('Doge', 'SUCH CORS');
+  let headers = Object.assign(removeKeys(req.headers, FORBIDDEN_CLIENT_HEADERS), {});
+  request(url,
+  {
+    headers: headers
+  }).on('response', (r) => { console.log("Finished POST (" + r.statusCode + ") " + url)}).pipe(res);
+});
+
 app.listen(3000, function () {
   console.log('CORS Running on port 3000!');
 });
