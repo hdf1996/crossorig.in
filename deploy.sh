3dcc60184d416cca8ea0abdd158378c92ac87413
hdf1986 <hugo.farji+github@gmail.com>
2019-07-11 00:26:35 -0300

    Updated build script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 # This will load the script from this repository. Make sure to point to a specific commit so the build continues to work
 # event if breaking changes are introduced in this repository
-source <(curl -s https://raw.githubusercontent.com/manastech/ci-docker-builder/3fee09cce08175cfd76a246dd95112686939fb9c/build.sh)
+source <(curl -s https://raw.githubusercontent.com/hdf1986/ci-docker-builder/master/build.sh)
 
 # Prepare the build
 dockerSetup
